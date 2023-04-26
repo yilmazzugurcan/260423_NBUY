fb27ff38365272264f25fe2c165a520e8e171f02
yilma <yilma@DESKTOP-U1LH654>
2023-04-26 21:26:56 +0300

    Add project files.

---
+++ 260423_NBUY/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace _260423_NBUY
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAd = new System.Windows.Forms.Label();
+            this.lblSoyad = new System.Windows.Forms.Label();
+            this.lblTlf = new System.Windows.Forms.Label();
+            this.lblMail = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dgvListe = new System.Windows.Forms.DataGridView();
+            this.CmbMusteri = new System.Windows.Forms.ComboBox();
+            this.cmbOda = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Location = new System.Drawing.Point(33, 36);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(20, 13);
+            this.lblAd.TabIndex = 0;
+            this.lblAd.Text = "Ad";
+            // 
+            // lblSoyad
+            // 
+            this.lblSoyad.AutoSize = true;
+            this.lblSoyad.Location = new System.Drawing.Point(33, 73);
+            this.lblSoyad.Name = "lblSoyad";
+            this.lblSoyad.Size = new System.Drawing.Size(37, 13);
+            this.lblSoyad.TabIndex = 1;
+            this.lblSoyad.Text = "Soyad";
+            
+            // 
+            // lblTlf
+            // 
+            this.lblTlf.AutoSize = true;
+            this.lblTlf.Location = new System.Drawing.Point(33, 104);
+            this.lblTlf.Name = "lblTlf";
+            this.lblTlf.Size = new System.Drawing.Size(43, 13);
+            this.lblTlf.TabIndex = 2;
+            this.lblTlf.Text = "Telefon";
+            // 
+            // lblMail
+            // 
+            this.lblMail.AutoSize = true;
+            this.lblMail.Location = new System.Drawing.Point(33, 137);
+            this.lblMail.Name = "lblMail";
+            this.lblMail.Size = new System.Drawing.Size(26, 13);
+            this.lblMail.TabIndex = 3;
+            this.lblMail.Text = "Mail";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(93, 101);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(93, 134);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 36);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(86, 223);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 36);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Güncelle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(149, 223);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(44, 36);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dgvListe
+            // 
+            this.dgvListe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListe.Location = new System.Drawing.Point(209, 33);
+            this.dgvListe.Name = "dgvListe";
+            this.dgvListe.Size = new System.Drawing.Size(561, 226);
+            this.dgvListe.TabIndex = 13;
+            // 
+            // CmbMusteri
+            // 
+            this.CmbMusteri.FormattingEnabled = true;
+            this.CmbMusteri.Location = new System.Drawing.Point(36, 306);
+            this.CmbMusteri.Name = "CmbMusteri";
+            this.CmbMusteri.Size = new System.Drawing.Size(129, 21);
+            this.CmbMusteri.TabIndex = 14;
+            // 
+            // cmbOda
+            // 
+            this.cmbOda.FormattingEnabled = true;
+            this.cmbOda.Location = new System.Drawing.Point(192, 306);
+            this.cmbOda.Name = "cmbOda";
+            this.cmbOda.Size = new System.Drawing.Size(129, 21);
+            this.cmbOda.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Müşteri";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 290);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Oda";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(347, 304);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Rezerve";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 643);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbOda);
+            this.Controls.Add(this.CmbMusteri);
+            this.Controls.Add(this.dgvListe);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblMail);
+            this.Controls.Add(this.lblTlf);
+            this.Controls.Add(this.lblSoyad);
+            this.Controls.Add(this.lblAd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListe)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.Label lblSoyad;
+        private System.Windows.Forms.Label lblTlf;
+        private System.Windows.Forms.Label lblMail;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dgvListe;
+        private System.Windows.Forms.ComboBox CmbMusteri;
+        private System.Windows.Forms.ComboBox cmbOda;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ 260423_NBUY/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _260423_NBUY
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        SqlConnection baglanti = new SqlConnection("Server=.;Database=Musteriler;User=sa;Pwd=123");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MusteriListele();
+            MusterileriGetir();
+            OdalariGetir();
+        }
+
+        private void MusteriListele()
+        {
+            SqlDataAdapter adp = new SqlDataAdapter("Select * from Musteriler", baglanti);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            dgvListe.DataSource = dt;
+
+        }
+        private void MusterileriGetir()
+        {
+            SqlDataAdapter adp = new SqlDataAdapter("SELECT MusteriID,Ad+' '+Soyad as AdSoyad FROM Musteriler", baglanti);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            CmbMusteri.DataSource = dt;
+            CmbMusteri.DisplayMember = "AdSoyad";
+            CmbMusteri.ValueMember = "MusteriID";
+        }
+        private void OdalariGetir()
+        {
+            SqlDataAdapter adp = new SqlDataAdapter("SELECT OdaNo,OdaFiyati FROM Oda", baglanti);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            cmbOda.DataSource = dt;
+            cmbOda.DisplayMember = "OdaNo";
+            cmbOda.ValueMember = "OdaFiyati";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string adi = textBox1.Text;
+            string soyad = textBox2.Text;
+            string telefon = textBox3.Text;
+            string mail = textBox4.Text;
+            if (adi == string.Empty || soyad == string.Empty || telefon == string.Empty || mail == string.Empty)
+            {
+                MessageBox.Show("Lütfen alanları doldurun.");
+            }
+            else
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = string.Format("insert musteriler(Ad,Soyad,Telefon,Mail) values ('{0}','{1}','{2}','{3}')", adi, soyad, telefon, mail);
+                cmd.Connection = baglanti;
+                baglanti.Open();
+                int etk = cmd.ExecuteNonQuery();
+                if (etk > 0)
+                {
+                    MessageBox.Show("Kayıt Eklendi.");
+                    MusteriListele();
+                }
+                else
+                {
+                    MessageBox.Show("Hata.");
+                }
+                baglanti.Close();
+                textBox1.Text = string.Empty;
+                textBox2.Text = string.Empty;
+                textBox3.Text = string.Empty;
+                textBox4.Text = string.Empty;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string adi = textBox1.Text;
+            string soyad = textBox2.Text;
+            string telefon = textBox3.Text;
+            string mail = textBox4.Text;
+            int musteriID = Convert.ToInt32(dgvListe.CurrentRow.Cells["MusteriID"].Value); // Seçilen satırdaki MüşteriID değerini al
+
+            SqlCommand komut = new SqlCommand();
+            komut.CommandText = string.Format("UPDATE musteriler SET Ad='{0}', Soyad='{1}', Telefon='{2}', Mail='{3}' WHERE MusteriID={4}", adi, soyad, telefon, mail, musteriID);
+            komut.Connection = baglanti;
+
+            try
+            {
+                baglanti.Open();
+                int etk = komut.ExecuteNonQuery();
+                if (etk > 0)
+                {
+                    MessageBox.Show("Güncellendi.");
+                    MusteriListele();
+                }
+                else
+                {
+                    MessageBox.Show("Güncelleme Başarısız.");
+                }
+                baglanti.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dgvListe.SelectedRows.Count == 0) // Hiçbir satır seçilmediyse
+            {
+                MessageBox.Show("Lütfen önce bir satır seçin.");
+                return;
+            }
+
+            int musteriID = Convert.ToInt32(dgvListe.CurrentRow.Cells["MusteriID"].Value); // Seçilen satırdaki MüşteriID değerini al
+
+            if (MessageBox.Show("Seçilen müşteri kaydını silmek istediğinize emin misiniz?", "Müşteri Silme", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                SqlCommand komut = new SqlCommand();
+                komut.CommandText = string.Format("DELETE FROM musteriler WHERE MusteriID={0}", musteriID);
+                komut.Connection = baglanti;
+
+                try
+                {
+                    baglanti.Open();
+                    int etk = komut.ExecuteNonQuery();
+                    if (etk > 0)
+                    {
+                        MessageBox.Show("Silindi.");
+                        MusteriListele();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Silme Başarısız.");
+                    }
+                    baglanti.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+ 
+        }
+    }
+}
+
